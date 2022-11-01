0ae1c82efec980b048605759dd9f83c9d8980287
Dilshard <mrdilshard@gmail.com>
2022-11-01 16:15:42 +0530

    Session 10

---
+++ Session10/insert.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create record</title>
+    <style>
+        input{
+            margin : 4px;
+        }
+    </style>
+</head>
+<body>
+
+    <form method="post">
+        <input type="text" name="fname" placeholder="Full Name"> <br>
+        <input type="text" name="gender" placeholder="Gender"> <br>
+        <input type="text" name="address" placeholder="Address"> <br>
+        <input type="text" name="uname" placeholder="User Name"> <br>
+        <input type="password" name="pass" placeholder="Password"> <br>
+        <input type="submit" name="submit" value="Save">
+
+    </form>
+
+    <?php 
+    include 'con.php'; 
+
+    if(isset($_POST['submit'])){
+        $fullName = $_POST['fname'];
+        $gender = $_POST['gender'];
+        $address = $_POST['address'];
+        $userName = $_POST['uname'];
+        $pass = $_POST['pass'];
+
+        $sql = "INSERT INTO `personal` (`fullname`, `gender`, `address`, `username`, `password`) VALUES ('$fullName', '$gender', '$address', '$userName', '$pass')";
+
+        $status = mysqli_query($con,$sql);
+
+        if($status){
+            echo "Successfully added!";
+        }else{
+            echo "Error - Add";
+        }
+    }
+
+
+    
+    ?>
+</body>
+</html>
